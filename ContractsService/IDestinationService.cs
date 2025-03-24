46f7080b792acc417f81cb3b68de9a85d6e69bf2
Francisco Echavarría <fjepaez@gmail.com>
2025-03-24 12:29:16 -0500

    fix user_Answers

--- ContractsService/IDestinationService.cs
+++ ContractsService/IDestinationService.cs
@@ -7,6 +7,7 @@ public interface IDestinationService
     // Task<IEnumerable<Destination>> GetAllDestinations();
     Task<Destination> GetDestinationById(int id);
     // Task<Destination> AddDestination(Destination destination);
+    // Task<Destination> AddDestination(Destination destination);
     // Task<Destination> UpdateDestination(Destination destination);
     // Task<Destination> DeleteDestination(int id);
     //string GetHashedArray(string[] array);    
